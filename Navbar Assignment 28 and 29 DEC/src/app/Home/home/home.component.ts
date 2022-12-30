c40483a12ecadf60ab205069669ed9bf8bc8aedb
Sahiramdara <sahi.ram@yash.com>
2022-12-30 12:01:36 +0530

    updated module

--- Navbar Assignment 28 and 29 DEC/src/app/Home/home/home.component.ts
+++ Navbar Assignment 28 and 29 DEC/src/app/Home/home/home.component.ts
@@ -5,6 +5,10 @@ export interface Hobbie
   name:string;
   color:ThemePalette;
 }
+export interface Section {
+  name: string;
+  updated: Date;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,9 +19,33 @@ export class HomeComponent {
   Hobbies:Hobbie[]=
   [
     {name:'Dance',color:undefined},
-    {name:'Writing',color:'primary'},
+    {name:'Writing',color:'accent'},
     {name:'Photography',color:'warn'},
     {name:'Shopping',color:'accent'},
    
+  ];
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    },
+  ];
+  notes: Section[] = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    },
   ];
 }
